a13e8c7df760669b923a892a8db449ba861e272c
Joshua Marangoni <45152087+JoshMarangoni@users.noreply.github.com>
2021-09-15 13:50:39 -0700

    Update Program.cs

--- SumOfDigits/Program.cs
+++ SumOfDigits/Program.cs
@@ -6,7 +6,7 @@ namespace SumOfDigits
     {
         static void Main(string[] args)
         {
-            Console.Write("Input  a number(integer): ");
+            Console.Write("Input a number(integer): ");
             int n = Convert.ToInt32(Console.ReadLine());
             int sum = 0;
             while (n != 0)
