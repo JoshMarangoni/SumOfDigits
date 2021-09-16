b8788a648163b738c1d5a7298be5632959ac43c8
Joshua Marangoni <joshua.marangoni@gmail.com>
2021-09-15 17:21:34 -0700

    Implement sum of even indexes

--- SumOfDigits/Program.cs
+++ SumOfDigits/Program.cs
@@ -8,11 +8,22 @@ namespace SumOfDigits
         {
             Console.Write("Input a number(integer): ");
             int n = Convert.ToInt32(Console.ReadLine());
-            int sum = 0;
-            while (n != 0)
+            int copy  = n;
+            int sum   = 0;
+            int count = 0;
+            while (copy != 0)
             {
-                sum += n % 10;
+                copy /= 10;
+                count++;
+            }
+            bool skip = (count % 2 == 0) ? true : false;
+            while(n != 0)
+            {
+                int digit = n % 10;
                 n /= 10;
+                if (skip) { skip = false; continue; }
+                sum += digit;
+                skip = true;
             }
             Console.WriteLine("Sum of the digits you entered is: " + sum);
             Console.WriteLine("Thank you.");
